5899a38ca7780b8c0dfefc1f635e88ea9ab33b35
Speyser <92744165+Speyser@users.noreply.github.com>
2025-02-13 09:12:12 +0000

    Initial commit of bot

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+# Завантаження змінних з .env
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: Message):
+    await message.answer(f"Ви сказали: {message.text}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
